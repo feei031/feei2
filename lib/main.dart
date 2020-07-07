640e46fce4b1576205d01d75b91aa3bee1b4d8b1
feei031 <arkt03111@gmail.com>
2020-07-07 15:45:57 +0900

    開発しようぜに変更

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('アプリ'),
+        title: Text('アプリ開発しようぜ'),
       ),
       body: Container(
         width: double.infinity,
