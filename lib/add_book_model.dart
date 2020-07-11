cfada4f66584d55f824c6455ab081a3f08203a82
feei031 <arkt03111@gmail.com>
2020-07-11 12:50:54 +0900

    firesbaseの値を更新

--- lib/add_book_model.dart
+++ lib/add_book_model.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import 'book.dart';
+
 class AddBookModel extends ChangeNotifier {
   String bookTitle = '';
 
@@ -12,6 +14,17 @@ class AddBookModel extends ChangeNotifier {
     Firestore.instance.collection('books').add(
       {
         'title': bookTitle,
+      },
+    );
+  }
+
+  Future updateBook(Book book) async {
+    final document =
+        Firestore.instance.collection('books').document(book.documentID);
+    await document.updateData(
+      {
+        'title': bookTitle,
+        'updateAt': Timestamp.now(),
       },
     );
   }
--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -21,6 +21,23 @@ class BookListPage extends StatelessWidget {
                 .map(
                   (book) => ListTile(
                     title: Text(book.title),
+                    trailing: IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () async {
+                        //todo
+
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddBookPage(
+                              book: book,
+                            ),
+                            fullscreenDialog: true,
+                          ),
+                        );
+                        model.fetchBooks();
+                      },
+                    ),
                   ),
                 )
                 .toList();
